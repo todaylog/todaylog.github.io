94540c6fd5c19f008a5034bcc3764db7a9dc0427
Eunbi Han <bird_@naver.com>
2023-05-23 22:40:35 +0900

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-const API_KEY = process.env.API_KEY;
+// const API_KEY = process.env.API_KEY;
+
+const debug = process.env.NODE_ENV !== 'production';
 
 module.exports = {
     reactStrictMode: true,
+    assetPrefix: !debug ? `/` : '', // production 일때 prefix 경로
+    trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+
     async redirects() {
         return [
             {
